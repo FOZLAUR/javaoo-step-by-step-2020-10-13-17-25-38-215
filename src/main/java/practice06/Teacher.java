449242ff49799cd198ce1a6f7351b994058f19ba
FOZLAUR <fozla@oocl.com>
2020-10-14 16:57:58 +0800

    [practice06][LaurenceFoz] feature: Added answers for practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() { return klass; }
+
+    @Override
+    public String introduce() {
+        if (klass > 0) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + klass + ".";
+        } else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+    }
 }
