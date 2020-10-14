78b61eae585968ab723930b1126e6a1521ec5ecd
FOZLAUR <fozla@oocl.com>
2020-10-14 15:57:48 +0800

    [practice04] feature: Added answers to practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,17 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    private int kClass;
+
+    public Student(String name, int age, int kClass) {
+        super(name, age);
+        this.kClass = kClass;
+    }
+
+    public int getKlass() { return kClass; }
+
+    @Override
+    public String introduce() {
+        return "My name is "+getName()+". I am "+getAge()+" years old. I am a Student. I am at Class "+kClass+".";
+    }
 }
