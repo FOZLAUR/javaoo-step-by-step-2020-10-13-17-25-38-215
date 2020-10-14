c454cb165128ff915df0a4c1e7d3a09eaf39ad93
FOZLAUR <fozla@oocl.com>
2020-10-14 20:19:19 +0800

    [practice08][LaurenceFoz] feature: Added answers for practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,23 @@
 package practice08;
 
 public class Klass {
+    private int number = -1;
+    private Student studentLeader;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() { return number; }
+
+    public String getDisplayName() { return "Class "+number; }
+
+    public void assignLeader(Student student) {
+        student.setLeader();
+        studentLeader = student;
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,28 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    private boolean isLeader;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        isLeader = false;
+    }
+
+    public Klass getKlass() { return klass; }
+
+
+    public void setLeader() {
+        isLeader = true;
+    }
+
+    @Override
+    public String introduce() {
+        if(isLeader) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        } else {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Student. I am at Class " + klass.getNumber() + ".";
+        }
+    }
 }
