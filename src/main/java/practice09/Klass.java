699d250290ac8db5b7952942fcfb3a6c2b8ced1f
FOZLAUR <fozla@oocl.com>
2020-10-14 21:21:14 +0800

    [practice09][LaurenceFoz] feature: Added answers for practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,40 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student studentLeader;
+    private List<Student> studentList;
+
+    public Klass(int number){
+        this.number = number;
+        studentList = new ArrayList<Student>();
+    }
+
+    public int getNumber() { return number; }
+
+    public String getDisplayName() { return "Class "+number; }
+
+    public void assignLeader(Student student) {
+        if(student.getKlass().getNumber() == number) {
+            student.setLeader();
+            studentLeader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public Student getLeader() {
+        return studentLeader;
+    }
+
+    public void appendMember(Student student) {
+        if(student.getKlass().getNumber() == number) {
+            studentList.add(student);
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
 }
