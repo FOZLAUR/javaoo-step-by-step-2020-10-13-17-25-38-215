487f094489e5f37226fb3fd720d4be617fd1ca00
FOZLAUR <fozla@oocl.com>
2020-10-14 22:47:46 +0800

    [practice10][LaurenceFoz] feature: Added answers for practice10

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,36 @@
 package practice10;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is "+name+". I am "+age+" years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Person)) {
+            return false;
+        }
+
+        Person c = (Person) o;
+
+        return Integer.compare(id, c.id) == 0 && name.equals(c.name) && Integer.compare(age, c.age) == 0;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,67 @@
 package practice10;
 
-public class Teacher {
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    private Klass klass;
+    private LinkedList<Klass> klassList;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+//    public Teacher(int id, String name, int age, Klass klass) {
+//        super(id, name, age);
+//        this.klass = klass;
+//    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klassList) {
+        super(id, name, age);
+        this.klassList = klassList;
+    }
+
+    public Klass getKlass() { return klass; }
+
+    @Override
+    public String introduce() {
+        if (klassList != null && klassList.size() > 0) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + returnClassString() + ".";
+        } else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (isTeaching(student)) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach "+student.getName()+".";
+        } else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klassList;
+    }
+
+    public String returnClassString(){
+        String str = "";
+        for(int i=0; i<klassList.size(); i++){
+            str+=klassList.get(i).getNumber();
+            if(i!=klassList.size()-1){
+                str+=", ";
+            }
+        }
+        return str;
+    }
+
+
+    public boolean isTeaching(Student student) {
+        for(Klass klass : klassList){
+            if(klass.isIn(student)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
