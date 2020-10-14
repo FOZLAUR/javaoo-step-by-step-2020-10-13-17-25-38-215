dd90f895e54980ca1fc27fce6395cd1750d57a9d
FOZLAUR <fozla@oocl.com>
2020-10-14 17:20:24 +0800

    [practice07][LaurenceFoz] feature: Added answers for practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,15 @@
 package practice07;
 
 public class Klass {
+
+    private int number = -1;
+
+    public Klass(int number){
+        this.number = number;
+    }
+
+    public int getNumber() { return number; }
+
+    public String getDisplayName() { return "Class "+number; }
+
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,33 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() { return klass; }
+
+    @Override
+    public String introduce() {
+        if (klass != null && klass.getNumber() > 0) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        } else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().equals(getKlass())) {
+            return "My name is " + getName() + ". I am " + getAge() + " years old. I am a Teacher. I teach "+student.getName()+".";
+        } else {
+            return "My name is Tom. I am 21 years old. I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
 }
